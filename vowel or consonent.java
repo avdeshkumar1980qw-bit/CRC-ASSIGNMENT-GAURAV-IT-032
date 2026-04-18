f1a83963a8a287a2a11f2a0f7c544d2a5e3548df
Gaurav pal <avdeshkumar1980.qw@gmail.com>
2026-04-18 11:00:14 +0530

    Add Vowel or Consonant checker in Java

---
+++ vowel or consonent.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        // Check if it's an alphabet
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+
+            // Check vowel
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+
+                System.out.println("Vowel");
+
+            } else {
+                System.out.println("Consonant");
+            }
+
+        } else {
+            System.out.println("Not an alphabet");
+        }
+    }
+}
